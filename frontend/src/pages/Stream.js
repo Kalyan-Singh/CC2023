fc7e40eb4a50d9bd7bb9c0e0caa54b7ded538c38
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-03-06 13:47:53 +0530

    updated

--- frontend/src/pages/Stream.js
+++ frontend/src/pages/Stream.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {Player, useCreateStream } from '@livepeer/react';
+import { Box, Button, Input } from '@chakra-ui/react';
 
 function Stream() {
+  const [streamName, setStreamName] = useState('');
+  
+  const {
+    mutate: createStream,
+    data: stream,
+    status,
+  } = useCreateStream({ name: streamName });
+
+  const handleInputChange = (event) => {
+    setStreamName(event.target.value);
+  }
+
   return (
-    <div>Stream</div>
-  )
+    <>
+      <Input
+        placeholder="Enter stream name"
+        value={streamName}
+        onChange={handleInputChange}
+      />
+      <Button
+        disabled={status === 'loading' || !createStream}
+        onClick={() => createStream?.()}
+      >
+        Create Stream
+      </Button>
+      {stream && <Box>Stream Key: {stream.streamKey}</Box>}
+      {stream && <Box>Playback ID: {stream.playbackId}</Box>}
+      {stream && <Player playbackId={stream.playbackId}/>}
+    </>
+  );
 }
 
-export default Stream
+export default Stream;
